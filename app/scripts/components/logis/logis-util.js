2d3a8d45f90dbb972f31b3b4439bfbfa3fa949cf
hatiolab <shortstop@hatiolab.com>
2019-10-18 16:37:19 +0900

    리팩토링

--- app/scripts/components/logis/logis-util.js
+++ app/scripts/components/logis/logis-util.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Source Name:    any-util.js
+ *  Source Name:    logis-util.js
  *  Description:    유틸리티
  *  Author:         남종호
  *  Update History:
@@ -7,90 +7,90 @@
  *
  ******************************************************************************/
 
-ANY_UTIL = {
+LOGIS_UTIL = {
 };
 
 /**
  * 디바이스 타입 조회
  */
-ANY_UTIL.getDeviceType = function() {
+LOGIS_UTIL.getDeviceType = function() {
   return JSON.parse(localStorage.getItem('setting.deviceType'));
 };
 
 /**
  * 대상 외 주문 보이기 여부 리턴
  */
-ANY_UTIL.getShowOthersOrder = function() {
+LOGIS_UTIL.getShowOthersOrder = function() {
   return JSON.parse(localStorage.getItem('setting.showOthersOrder'));
 };
 
 /**
  * 현재 태블릿의 호기 코드 리턴
  */
-ANY_UTIL.getRegionCd = function() {
+LOGIS_UTIL.getRegionCd = function() {
   return JSON.parse(localStorage.getItem('setting.regionCode'));
 };
 
 /**
  * 현재 태블릿의 호기명을 리턴
  */
-ANY_UTIL.getRegionNm = function() {
+LOGIS_UTIL.getRegionNm = function() {
   return JSON.parse(localStorage.getItem('setting.regionName'));
 };
 
 /**
  * 현재 태블릿의 조회 범위 존을 리턴
  */
-ANY_UTIL.getZoneCd = function() {
+LOGIS_UTIL.getZoneCd = function() {
   return JSON.parse(localStorage.getItem('setting.zoneCode'));
 };
 
 /**
  * 현재 태블릿의 작업 위치를 리턴
  */
-ANY_UTIL.getSideCd = function() {
+LOGIS_UTIL.getSideCd = function() {
   return JSON.parse(localStorage.getItem('setting.workLocation'));
 };
 
 /**
  * 작업 위치를 로컬 스토리지에서 조회
  */
-ANY_UTIL.getWorkLoc = function() {
+LOGIS_UTIL.getWorkLoc = function() {
   return JSON.parse(localStorage.getItem('setting.workLocation'));
 };
 
 /**
  * 현재 태블릿의 자동피킹 여부를 리턴
  */
-ANY_UTIL.getAutoPicking = function() {
+LOGIS_UTIL.getAutoPicking = function() {
   return JSON.parse(localStorage.getItem('setting.autoPicking'));
 };
 
 /**
  * 현재 태블릿의 상품코드 전부 보기 여부를 리턴
  */
-ANY_UTIL.getShowFullCode = function() {
+LOGIS_UTIL.getShowFullCode = function() {
   return JSON.parse(localStorage.getItem('setting.showFullCode'));
 };
 
 /**
  * 표시할 송장 번호 문자열의 시작 인덱스를 리턴
  */
-ANY_UTIL.getInvoiceFieldSubstr = function () {
+LOGIS_UTIL.getInvoiceFieldSubstr = function () {
   return parseInt(JSON.parse(localStorage.getItem('setting.invoiceFieldSubstr')));
 };
 
 /**
  * 작업 유형 리턴
  */
-ANY_UTIL.getJobName = function () {
+LOGIS_UTIL.getJobName = function () {
   return JSON.parse(localStorage.getItem('setting.jobName'));
 };
 
 /**
  * 데이터 조회 주기
  */
-ANY_UTIL.getRefreshInterval = function() {
+LOGIS_UTIL.getRefreshInterval = function() {
   let interval = JSON.parse(localStorage.getItem('setting.kioskInterval'));
   interval = Number(interval) * 1000;
   return interval;
@@ -99,49 +99,49 @@ ANY_UTIL.getRefreshInterval = function() {
 /**
  * 현재 키오스크 코드 타입을 리턴
  */
-ANY_UTIL.getKioskCodeType = function() {
+LOGIS_UTIL.getKioskCodeType = function() {
   return JSON.parse(localStorage.getItem('setting.kioskCodeType'));
 };
 
 /**
  * 현재 키오스크 Input 타입을 리턴
  */
-ANY_UTIL.getKioskInputType = function() {
+LOGIS_UTIL.getKioskInputType = function() {
   return JSON.parse(localStorage.getItem('setting.kioskInputType'));
 };
 
 /**
  * 현재 키오스크 프린트 아이디 리턴
  */
-ANY_UTIL.getKioskPrinterId = function() {
+LOGIS_UTIL.getKioskPrinterId = function() {
   return JSON.parse(localStorage.getItem('setting.kioskPrinterId'));
 };
 
 /**
  * 브로커 사이트 조회
  */
-ANY_UTIL.getBrokerSiteCd = function() {
+LOGIS_UTIL.getBrokerSiteCd = function() {
   return localStorage.getItem('setting.brokerSite');
 };
 
 /**
  * 브로커 주소 조회
  */
-ANY_UTIL.getBrokerAddress = function() {
+LOGIS_UTIL.getBrokerAddress = function() {
   return localStorage.getItem('setting.brokerAddress');
 };
 
 /**
  * 브로커 포트 조회
  */
-ANY_UTIL.getBrokerPort = function () {
+LOGIS_UTIL.getBrokerPort = function () {
   return localStorage.getItem('setting.brokerPort');
 };
 
 /**
  * 연속 스캔 허용 여부 조회
  */
-ANY_UTIL.isContinousScanAllowed = function () {
+LOGIS_UTIL.isContinousScanAllowed = function () {
   let continousScanAllowed = localStorage.getItem('setting.continousScanAllowed');
   return continousScanAllowed === null ? false : continousScanAllowed;
 };
@@ -153,7 +153,7 @@ ANY_UTIL.isContinousScanAllowed = function () {
  * @param {String} message
  * @param {Function} confirmCallback
  */
-ANY_UTIL.showMessage = function(title, message, confirmCallback) {
+LOGIS_UTIL.showMessage = function(title, message, confirmCallback) {
   openAlert({
     title: title,
     message: message,
@@ -168,7 +168,7 @@ ANY_UTIL.showMessage = function(title, message, confirmCallback) {
  * @param {String} message
  * @param {Function} confirmCallback
  */
-ANY_UTIL.showConfirm = function(title, message, cancelCallback, confirmCallback) {
+LOGIS_UTIL.showConfirm = function(title, message, cancelCallback, confirmCallback) {
   openConfirm({
     title: title,
     message: message,
@@ -180,8 +180,8 @@ ANY_UTIL.showConfirm = function(title, message, cancelCallback, confirmCallback)
 /**
  * 설정이 비어있는 경우 핸들러
  */
-ANY_UTIL.handleRequiredSettingEmpty = function() {
-  ANY_UTIL.showMessage(t('text.selecting_region'), t('text.select_region'), function() {
+LOGIS_UTIL.handleRequiredSettingEmpty = function() {
+  LOGIS_UTIL.showMessage(t('text.selecting_region'), t('text.select_region'), function() {
     location.hash = '/any_setting';
   });
 };
@@ -189,9 +189,9 @@ ANY_UTIL.handleRequiredSettingEmpty = function() {
 /**
  * 설정에 호기 설정이 비어있는 지 체크
  */
-ANY_UTIL.checkRequiredSettingEmpty = function() {
-  if(!ANY_UTIL.getRegionCd()) {
-    ANY_UTIL.handleRequiredSettingEmpty();
+LOGIS_UTIL.checkRequiredSettingEmpty = function() {
+  if(!LOGIS_UTIL.getRegionCd()) {
+    LOGIS_UTIL.handleRequiredSettingEmpty();
     return false;
   } else {
     return true;
@@ -208,7 +208,7 @@ ANY_UTIL.checkRequiredSettingEmpty = function() {
  * @param {Function} openCallback 팝업 오픈 때 실행 될 콜백 함수
  * @param {Function} closeCallback 팝업 닫을 때 실행 될 콜백 함수
  */
-ANY_UTIL.showPopup = function(title, popup, width, height, openCallback, closeCallback) {
+LOGIS_UTIL.showPopup = function(title, popup, width, height, openCallback, closeCallback) {
   document.dispatchEvent(new CustomEvent('open-dialog', {
     detail: {
       content: popup,
@@ -235,7 +235,7 @@ ANY_UTIL.showPopup = function(title, popup, width, height, openCallback, closeCa
  * @param {String} sideCd
  * @param {String} screen
  */
-ANY_UTIL.setSideCd = function(sideCd, screen) {
+LOGIS_UTIL.setSideCd = function(sideCd, screen) {
   if(!sideCd) sideCd = 'F';
 
   screen.showFront = false;
@@ -259,7 +259,7 @@ ANY_UTIL.setSideCd = function(sideCd, screen) {
  * 다국어 처리된 완료 상태 값
  ******************
  */
-ANY_UTIL.getFinishedStatusName = function() {
+LOGIS_UTIL.getFinishedStatusName = function() {
   return t('label.finished');
 };
 
@@ -267,7 +267,7 @@ ANY_UTIL.getFinishedStatusName = function() {
  * 다국어 처리된 미완료 상태 값
  ******************
  */
-ANY_UTIL.getUnfinishedStatusName = function() {
+LOGIS_UTIL.getUnfinishedStatusName = function() {
   return t('label.unfinished');
 };
 
@@ -276,7 +276,7 @@ ANY_UTIL.getUnfinishedStatusName = function() {
  ******************
  * @param items
  */
-ANY_UTIL.sortByLeftQty = function(items) {
+LOGIS_UTIL.sortByLeftQty = function(items) {
   items.sort(function(a, b) {
     if(a.left_qty == b.left_qty) return 0;
     else if(a.left_qty > 0 && b.left_qty == 0) return -1;
